a22d96ad2985011df6a92908b7588e90852afc92
cooper09 <cooper@Tupac.local>
2014-10-15 11:58:41 -0400

    initial angular load app

---
+++ app.js
@@ -0,0 +1,12 @@
+angular.module('loadApp',[]);
+
+/*$scope.add = function(){
+  alert("add me, baby!");
+  var f = document.getElementById('file').files[0],
+      r = new FileReader();
+  r.onloadend = function(e){
+    var data = e.target.result;
+    //send you binary data via $http or $resource or do anything else with it
+  }
+  r.readAsBinaryString(f);
+} */
